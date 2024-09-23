45dac62648e127ea3b815fdc16bb7708e3f7d022
Renato Negrete <negreterenato@gmail.com>
2024-09-23 07:35:01 -0500

    cargar usando clases

--- src/Solver.java
+++ src/Solver.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class Solver {
 
@@ -12,7 +13,22 @@ public class Solver {
         try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
             String clausula;
             while ((clausula = br.readLine()) != null) {
-                clausulas.add(clausula.trim()); // Agrega la línea a la lista, eliminando espacios en blanco
+                StringTokenizer st = new StringTokenizer(clausula, "v");
+                while (st.hasMoreTokens()) {
+                    String token = st.nextToken().trim();
+                    int inicio = token.indexOf('(');
+                    String predicado = token.substring(0, inicio);
+                    System.out.println("Predicado: " + predicado);
+                    inicio++;
+                    int fin = token.indexOf(')');
+                    String args = token.substring(inicio, fin);
+                    String[] arguments = args.split(",");
+                    String arg1 = arguments[0].trim();
+                    String arg2 = arguments.length > 1 ? arguments[1].trim() : null;
+                    System.out.println("Arg1: " + arg1 + "; Arg2: " + arg2);
+
+                }
+                //clausulas.add(clausula.trim()); Agrega la línea a la lista, eliminando espacios en blanco
             }
         } catch (IOException e) {
             System.err.println("Error al leer el archivo: " + e.getMessage());
